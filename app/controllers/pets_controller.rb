ea935244f3a30db406c951c123536b02bd9a4b33
cos404 <22division7@gmail.com>
2017-08-29 03:20:54 +0300

    Add: main views and controllers

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,21 @@
+class PetsController < ApplicationController
+  def index
+    @pets = Pet.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+
+  before_action :find_user, only: [:show, :edit, :update]
+
+  def index
+    @users = User.all
+  end
+
+  def edit
+  end
+
+  def update
+    @user.update_attributes(user_params)
+
+    if @user.errors.empty?
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :status, :avatar, :fullname, :about)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+end
