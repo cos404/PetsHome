0f2fd81bfe15eb3cbf5aa978d08d8db9a632c1b8
cos404 <22division7@gmail.com>
2017-09-11 18:32:52 +0300

    Update: Home page(controller and view)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,14 @@ class HomeController < ApplicationController
 
 
   def index
-    @shelters = root_url + 'shelters.json'
+    ip = '37.214.180.224'                 #
+    @geo = Geocoder.search(ip)[0].data    #
+    @shelters = root_url + 'shelters.json'#
 
-    ip = '37.214.180.224'
-    @geo = Geocoder.search(ip)[0].data
-
+    @pets = Pet.select(:id, :name, :age, "cities.title_#{I18n.locale} AS city").joins(shelter: :city).limit(8)
     render layout: "home_layout"
   end
 
 
 end
+
